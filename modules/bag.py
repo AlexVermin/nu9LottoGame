8adddaa23180796abecf13b5df591659ca620597
AlexVermin <alex_vermin@ngs.ru>
2019-12-19 23:17:59 +0700

    добавлено немного магии вуду

--- modules/bag.py
+++ modules/bag.py
@@ -20,7 +20,7 @@ class LottoBag:
 
     def get_number(self):
         """
-        Возвращает очередной случаййный бочонок или -1, если бочонки закончились
+        Возвращает очередной случайный бочонок или -1, если бочонки закончились
         :return: int
         """
         m_ret = None
@@ -42,13 +42,50 @@ class LottoBag:
         """
         return self.count
 
+    def __str__(self):
+        return f'Класс "Мешочек с бочёнками". Осталось {self.count} бочёнков.'
+
+    def __eq__(self, other):
+        is_equal = self.count == other.count
+        if is_equal:
+            for i in range(1, 91):
+                if i in self.numbers and i in other.numbers:
+                    is_equal = self.numbers[i] == other.numbers[i]
+                else:
+                    if i not in self.numbers and i not in other.numbers:
+                        is_equal = True
+                if not is_equal:
+                    break
+        return is_equal
+
+    def __gt__(self, other):
+        return self.count > other.count
+
+    def __ge__(self, other):
+        return self.count >= other.count
+
+    def __le__(self, other):
+        return self.count <= other.count
+
+    def __lt__(self, other):
+        return self.count < other.count
+
 
 if '__main__' == __name__:
     m_bag = LottoBag()
+    print(m_bag)
+    m_bag1 = LottoBag()
+    print('After init:')
+    print(f'  == : {m_bag == m_bag1}')
+    print(f'  != : {m_bag != m_bag1}')
     m_bag.shuffle()
-    print(m_bag.numbers)
-    print(f'Осталось {m_bag.count} бочонков')
-    for i in range(10):
-        print(m_bag.get_number())
-    print(m_bag.numbers)
-    print(f'Осталось {m_bag.count} бочонков')
+    m_bag1.shuffle()
+    print('After shuffle:')
+    print(f'  == : {m_bag == m_bag1}')
+    print(f'  != : {m_bag != m_bag1}')
+    for t in range(10):
+        m_bag.get_number()
+        m_bag1.get_number()
+    print('After 10 moves:')
+    print(f'  == : {m_bag == m_bag1}')
+    print(f'  != : {m_bag != m_bag1}')
