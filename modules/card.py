6fd061a58eb85d4602a6df2789e64ef83e167488
AlexVermin <alex_vermin@ngs.ru>
2019-12-19 23:18:10 +0700

    добавлено немного магии вуду

--- modules/card.py
+++ modules/card.py
@@ -50,6 +50,31 @@ class LottoCard:
             self.rows[m_row-1][p_number] = 1
             self.rest_num -= 1
 
+    def __str__(self):
+        return f'Класс "Карта для игры в лото". Осталось {self.rest_num} незачёркнутых чисел.'
+
+    def __eq__(self, other):
+        is_equal = self.rest_num == other.rest_num
+        if is_equal:
+            for row in range(3):
+                for num in self.rows[row]:
+                    is_equal = (other.has_number(num) != 0)
+                    if not is_equal:
+                        break
+        return is_equal
+
+    def __gt__(self, other):
+        return self.rest_num > other.rest_num
+
+    def __ge__(self, other):
+        return self.rest_num >= other.rest_num
+
+    def __le__(self, other):
+        return self.rest_num <= other.rest_num
+
+    def __lt__(self, other):
+        return self.rest_num < other.rest_num
+
 
 if '__main__' == __name__:
     m_card = LottoCard()
@@ -57,3 +82,13 @@ if '__main__' == __name__:
     print(m_card.has_number(90))
     m_card.mark(25)
     print(m_card.rows, m_card.rest_num)
+    m_add = LottoCard()
+    print(m_add.rows, m_add.rest_num)
+    print(m_card == m_add)
+    print(m_card != m_add)
+    m_test = m_card
+    print(m_card.rows, m_test.rest_num)
+    print(m_card.rows, m_test.rest_num)
+    print(m_card == m_test)
+    print(m_card != m_test)
+    print(list(m_card.rows[0].keys())[0])
